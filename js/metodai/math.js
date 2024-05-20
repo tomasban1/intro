6a755bddeecd6d85fc6d934a6c6236cba1e66cac
tomasban1 <tomasbanius@gmail.com>
2024-05-20 14:45:41 +0300

    math metodai

---
+++ js/metodai/math.js
@@ -0,0 +1,71 @@
+console.log(Math.E);
+console.log(Math.PI);
+console.log(Math.SQRT2);
+
+
+//Metodai(kaip funkcija)
+
+console.log(Math.abs(7));  //<----Duoto skaiciaus teigiama verte
+console.log(Math.abs(-7));
+console.log(Math.abs(3.14));
+
+console.log('--------------');
+
+console.log(Math.cbrt(8));//<-------Grazina kvadratine skaiciaus sakni
+console.log(Math.cbrt(27));
+console.log(Math.cbrt(64));
+console.log('----------------');
+
+console.log(Math.ceil(5));//<----------Skaiciaus apvalinimas i virsu
+console.log(Math.ceil(0));
+console.log(Math.ceil(-5));
+console.log(Math.ceil(3.14));
+console.log('---------------');
+
+console.log(Math.floor(3.14));//<------------Apvalina skaicius i apacia
+console.log(Math.floor(-3.14));//<----(-4)
+console.log(Math.floor(7.4));
+console.log('---------------');
+
+console.log(Math.round(3.0000001));//Matematinis apvalinimas(standartiskai)
+console.log(Math.round(-3.14));
+console.log(Math.round(3.4999999));
+console.log(Math.round(3.5000001));
+console.log(Math.round(3.999999));
+console.log('----------------');
+
+console.log(Math.trunc());
+console.log(Math.trunc(3.0000001));//nuima skaicius po kablelio
+console.log(Math.trunc(-3.14));
+console.log(Math.trunc(3.4999999));
+console.log(Math.trunc(3.5000001));
+console.log(Math.trunc(3.999999));
+console.log('-----------------');
+
+console.log(Math.exp(2));
+console.log('-------------');
+
+console.log(Math.fround(5));
+console.log(Math.fround(-5));
+console.log(Math.fround(3.14));
+console.log(Math.fround(-3.14));
+console.log(Math.fround(5));
+console.log('-------------');
+
+console.log(Math.hypot());//pitagoro teorema
+console.log('---------------');
+
+console.log(Math.imul(10, 17));//Daugyba 32 bit
+console.log('------------');
+
+console.log(Math.max(1, 3));//Isrenka is visu skaiciu didziausia skaiciu(ne masyvas)
+console.log(Math.min(1, 3));//Isrenka is visu skaiciu maziausia(ne masyvas)
+console.log('-----------');
+
+console.log(Math.pow(2, 3));//Pakelia pirma skaiciu antrojo skaiciaus laipsniu
+
+console.log(Math.random());//Grazina sugeneruota random skaiciu nuo 0 iki 1(imtinai 0, neimtinai 1)
+console.log(Math.random());
+console.log(Math.random());
+
+
