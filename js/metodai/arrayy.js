7510501bc6a9b6409d8672490fc51c725d3d74c6
tomasban1 <tomasbanius@gmail.com>
2024-05-28 19:33:27 +0300

    map ir sort metodai

--- js/metodai/arrayy.js
+++ js/metodai/arrayy.js
@@ -54,6 +54,79 @@ console.log(marks4.unshift(3));
 console.log(marks4);
 
 console.log([...marks].reverse());// apsuka array is kitos puses
+console.clear();
+//sudetingesni array metodai
 
+const a1 = [4, 6, 8, 10]
+console.log(a1.map(n => n * 2));
+console.log(['Labas', 'rytas', 'Lietuva'].map(s => s.length));//map sukuria cikla savyje ir isrenka reiksmes
 
+const firstSymbol = s => s[0];
+const lastSymbol = s => s[s.length - 1];
+console.log(['Labas', 'rytas', 'Lietuva'].map(firstSymbol));
+console.log(['Labas', 'rytas', 'Lietuva'].map(lastSymbol));
 
+const marks1 = [10, 2, 9, 6, 7];
+const updatedMarks = marks1.map(n => n + 1 > 10 ? 10 : n + 1);
+console.log(updatedMarks);
+
+function increaseMarks(marks) {
+    if (marks === 10) {
+        return 10
+    }
+    return marks + 1
+}
+
+const updatedMarks2 = marks1.map(increaseMarks);
+console.log(updatedMarks2);
+console.clear();
+
+const randomNum = [15, -7, 555, -1, 3.14]; //skaiciu isrinkimas su filter
+const positiveNumbers = randomNum.filter(n => n >= 0)
+console.log(positiveNumbers);
+const negativeNumbers = randomNum.filter(n => n < 0)
+console.log(negativeNumbers);
+
+console.log('------------------');
+const randomData = [14, -3.14, true, 18, undefined, [], false];
+//teigiami skaiciai
+const ts = randomData
+    .filter(n => Number.isInteger(n))
+    .filter(n => n > 0);
+
+console.log(ts);
+//SORT
+
+const dictionary = ['Labas', 'rytas', 'asla', 'medis', 'zuvis'];
+dictionary.sort();
+console.log(dictionary);
+
+//jei -1 a eina i prieki
+//jei 0 a ir b lieka vietoje
+// jei 1 a eina i gala
+const jonasMarks = [10, 8, 6, 2, 9];
+jonasMarks.sort((a, b) => b - a); // skirstymas didejimo, mazejimo tvarka
+console.log(jonasMarks);
+
+const students = [
+    { name: 'Jonas', marks: 7 },
+    { name: 'Maryte', marks: 9 },
+    { name: 'Petras', marks: 5 },
+    { name: 'Ona', marks: 2 },
+];
+
+students.sort((a, b) => a.marks - b.marks);// rusiavimas pagal pazymius
+console.log(students);
+
+function varduPalyginimas(a, b) {
+    if (a.name < b.name) {
+        return -1
+    } else if (a.name === b.name) {
+        return 0
+    } else {
+        return 1
+    }
+}
+
+students.sort(varduPalyginimas)
+console.log(students);
